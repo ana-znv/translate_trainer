748e1682dc12dd490c7ccf086ef4b0b793f8c2af
ana-znv <uzanzaev44@gmail.com>
2025-07-19 20:49:30 +0600

    created test button to send data

--- lib/features/add_sentence/view/add_sentence.dart
+++ lib/features/add_sentence/view/add_sentence.dart
@@ -8,6 +8,8 @@ class AddSentence extends StatefulWidget {
 }
 
 class _AddSentenceState extends State<AddSentence> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
@@ -15,47 +17,63 @@ class _AddSentenceState extends State<AddSentence> {
       appBar: AppBar(
         title: Text("Add Sentence", style: theme.textTheme.titleMedium),
       ),
-      body: Column(
-        children: [
-          Padding(padding: EdgeInsets.only(top: 40)),
-          Container(
-            alignment: Alignment.topCenter,
-            child: SizedBox(
-              width: 400,
-              child: TextField(
-                autocorrect: false,
-                enableSuggestions: false,
-                maxLines: null,
-                keyboardType: TextInputType.multiline,
-                style: theme.textTheme.labelMedium,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Native Sentence',
-                  hintStyle: theme.textTheme.labelSmall,
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            Padding(padding: EdgeInsets.only(top: 40)),
+            Container(
+              alignment: Alignment.topCenter,
+              child: SizedBox(
+                width: 400,
+                child: TextField(
+                  autocorrect: false,
+                  enableSuggestions: false,
+                  maxLines: null,
+                  keyboardType: TextInputType.multiline,
+                  style: theme.textTheme.labelMedium,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Native Sentence',
+                    hintStyle: theme.textTheme.labelSmall,
+                  ),
                 ),
               ),
             ),
-          ),
-          Padding(padding: EdgeInsets.only(top: 20)),
-          Container(
-            alignment: Alignment.topCenter,
-            child: SizedBox(
-              width: 400,
-              child: TextField(
-                autocorrect: false,
-                enableSuggestions: false,
-                maxLines: null,
-                keyboardType: TextInputType.multiline,
-                style: theme.textTheme.labelMedium,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Foreign Sentence',
-                  hintStyle: theme.textTheme.labelSmall,
+            Padding(padding: EdgeInsets.only(top: 20)),
+            Container(
+              alignment: Alignment.topCenter,
+              child: SizedBox(
+                width: 400,
+                child: TextField(
+                  autocorrect: false,
+                  enableSuggestions: false,
+                  maxLines: null,
+                  keyboardType: TextInputType.multiline,
+                  style: theme.textTheme.labelMedium,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Foreign Sentence',
+                    hintStyle: theme.textTheme.labelSmall,
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16),
+              child: ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('Data processing')),
+                    );
+                  }
+                },
+                child: const Text("Send"),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
